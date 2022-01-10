2a221759217e5159dd881f6b874cc84770990862
shogz <michaelbrill1@gmail.com>
2022-01-10 17:34:34 +0100

    ...

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ num_pixels = 15
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
+ORDER = neopixel.RGB
 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
