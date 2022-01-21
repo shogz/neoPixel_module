edde9a221e98088606551b0e420c14d5cfe4fd36
shogz <michaelbrill1@gmail.com>
2022-01-21 22:39:47 +0100

    ...

--- TR590_MkIX_animation.py
+++ TR590_MkIX_animation.py
@@ -3,11 +3,14 @@
 import pixelCore
 
 abgdState1 = pixelCore.PixelState([(0, (0,255,0)), (1, (0,0,0)), (2, (0,0,0)), (3, (0,0,0))], None, pixelCore.Transition(0.25))
-abgdState2 = pixelCore.PixelState([(0, (0,0,0)), (1, (0,255,0)), (2, (0,0,0)), (3, (0,0,0))], block1State3, pixelCore.Transition(0.25))
-abgdState3 = pixelCore.PixelState([(0, (0,0,0)), (1, (0,0,0)), (2, (0,255,0)), (3, (0,0,0))], block1State3, pixelCore.Transition(0.25))
-abgdState4 = pixelCore.PixelState([(0, (0,0,0)), (1, (0,0,0)), (2, (0,0,0)), (3, (0,255,0))], block1State3, pixelCore.Transition(0.25))
+abgdState2 = pixelCore.PixelState([(0, (0,0,0)), (1, (0,255,0)), (2, (0,0,0)), (3, (0,0,0))], None, pixelCore.Transition(0.25))
+abgdState3 = pixelCore.PixelState([(0, (0,0,0)), (1, (0,0,0)), (2, (0,255,0)), (3, (0,0,0))], None, pixelCore.Transition(0.25))
+abgdState4 = pixelCore.PixelState([(0, (0,0,0)), (1, (0,0,0)), (2, (0,0,0)), (3, (0,255,0))], None, pixelCore.Transition(0.25))
 
 abgdState1.setNext(abgdState2)
+abgdState2.setNext(abgdState3)
+abgdState3.setNext(abgdState4)
+abgdState4.setNext(abgdState1)
 
 abdgAnim = pixelCore.AnimationBlock("ABDG_ANIM")    
 abdgAnim.setCurrentState(abgdState1)
