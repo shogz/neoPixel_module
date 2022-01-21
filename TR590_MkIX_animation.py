87f720d89dad3768157edf71739060c624e12719
shogz <michaelbrill1@gmail.com>
2022-01-21 23:32:27 +0100

    Top Panel animations

--- TR590_MkIX_animation.py
+++ TR590_MkIX_animation.py
@@ -17,17 +17,17 @@ abgdState4.setNext(abgdState1)
 abdgAnim = pixelCore.AnimationBlock("ABDG_ANIM")    
 abdgAnim.setCurrentState(abgdState1)
 
-geoBTN = pixelCore.PixelState([(4, (255,150,0))], None, None)
+geoBTN = pixelCore.PixelState([(4, (255,50,0))], None, None)
 
 geoBTNAnim = pixelCore.AnimationBlock("GEO_BTN_ANIM")    
 geoBTNAnim.setCurrentState(geoBTN)
 
-metBTN = pixelCore.PixelState([(5, (255,150,0))], None, None)
+metBTN = pixelCore.PixelState([(5, (255,50,0))], None, None)
 
 metBTNAnim = pixelCore.AnimationBlock("MET_BTN_ANIM")    
 metBTNAnim.setCurrentState(metBTN)
 
-bioBTN = pixelCore.PixelState([(6, (255,150,0))], None, None)
+bioBTN = pixelCore.PixelState([(6, (255,50,0))], None, None)
 
 bioBTNAnim = pixelCore.AnimationBlock("BIO_BTN_ANIM")    
 bioBTNAnim.setCurrentState(bioBTN)
@@ -43,6 +43,24 @@ stateAnim3.setNext(stateAnim1)
 stateAnim = pixelCore.AnimationBlock("STATE_ANIM")    
 stateAnim.setCurrentState(stateAnim1)
 
+powerAnimState = pixelCore.PixelState([(11, (255,0,0)), (12, (0,255,0)), (13, (0,255,0)), (14, (0,255,0))], None, None)
+
+powerAnim = pixelCore.AnimationBlock("POWER_ANIM")    
+powerAnim.setCurrentState(powerAnimState)
+
+abgd2State1 = pixelCore.PixelState([(15, (255,0,0)), (16, (255,0,0)), (17, (0,255,0)), (18, (0,0,0)), (19, (0,0,0)), (20, (0,0,0))], None, pixelCore.Transition(0.25))
+abgd2State2 = pixelCore.PixelState([(15, (255,0,0)), (16, (255,0,0)), (17, (0,0,0)), (18, (0,255,0)), (19, (0,0,0)), (20, (0,0,0))], None, pixelCore.Transition(0.25))
+abgd2State3 = pixelCore.PixelState([(15, (255,0,0)), (16, (255,0,0)), (17, (0,0,0)), (18, (0,0,0)), (19, (0,255,0)), (20, (0,0,0))], None, pixelCore.Transition(0.25))
+abgd2State4 = pixelCore.PixelState([(15, (255,0,0)), (16, (255,0,0)), (17, (0,0,0)), (18, (0,0,0)), (19, (0,0,0)), (20, (0,255,0))], None, pixelCore.Transition(0.25))
+
+abdg2Anim = pixelCore.AnimationBlock("ABDG_2_ANIM")    
+abdg2Anim.setCurrentState(abgd2State1)
+
+abgd2State1.setNext(abgd2State2)
+abgd2State2.setNext(abgd2State3)
+abgd2State3.setNext(abgd2State4)
+abgd2State4.setNext(abgd2State1)
+
 pixelBlock = pixelCore.PixelBlock(board.D12 , 15, 0.2, True, neopixel.GRB)
 
 pixelBlock.addAnimationBlock(abdgAnim)
@@ -50,5 +68,7 @@ pixelBlock.addAnimationBlock(geoBTNAnim)
 pixelBlock.addAnimationBlock(metBTNAnim)
 pixelBlock.addAnimationBlock(bioBTNAnim)
 pixelBlock.addAnimationBlock(stateAnim)
+pixelBlock.addAnimationBlock(powerAnim)
+pixelBlock.addAnimationBlock(abdg2Anim)
 
 pixelBlock.startAll()
